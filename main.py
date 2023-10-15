2c1f8417b6cd6b47134698dfaa8f00d3aecc4798
solmaz-sahmani <solmaz.sahmani@gmail.com>
2023-10-15 21:04:20 +0330

    Day 3: match case and for loop

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-user_prompt = "Enter a todo:"
- 
-todos=[]
+todos = []
+
 
 while True:
-    todo = input(user_prompt)
-    print(todo.title()) 
-    todos.append(todo)
+    user_action = input("Type add, show ot exit: ")
+    user_action = user_action.strip()
 
-    
+    match user_action:
+        case "add":
+            todo = input("Enter a todo:")
+            todos.append(todo)
+        case "show" | "display":
+            for item in todos:
+                item = item.title()
+                print(item)
+        case "exit":
+            break
+        case _:
+            print("You entered an unknown command.")
+
+print("Bye!")
